7ac419a59991e2c15c3e9fce8c927783faee7fa0
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:32:13 +0900

    Vsync関係の設定を追加し、GraphicsDeviceは1番目ではなくデフォルトのものを取得するように

--- desktop/src/com/tres/TresApplicationSettings.java
+++ desktop/src/com/tres/TresApplicationSettings.java
@@ -8,11 +8,16 @@ public class TresApplicationSettings {
 
 	protected int foregroundFPS;
 
+	protected boolean vsync;
+
 	private final float refreshRate;
+
+	private final int width;
+	private final int height;
 
 	public TresApplicationSettings() {
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		GraphicsDevice device = ge.getScreenDevices()[0];
+		GraphicsDevice device = ge.getDefaultScreenDevice();
 
 		float refreshRate = device.getDisplayMode().getRefreshRate();
 		if (refreshRate == DisplayMode.REFRESH_RATE_UNKNOWN) {
@@ -21,7 +26,19 @@ public class TresApplicationSettings {
 			this.refreshRate = refreshRate;
 		}
 
+		this.width = device.getDisplayMode().getWidth();
+		this.height = device.getDisplayMode().getHeight();
+
 		this.foregroundFPS = (int) this.refreshRate;
+		this.vsync = false;
+	}
+
+	public int getMonitorHeight() {
+		return height;
+	}
+
+	public int getMonitorWidth() {
+		return width;
 	}
 
 	public float getMonitorRefreshRate() {
@@ -32,6 +49,15 @@ public class TresApplicationSettings {
 		this.foregroundFPS = fps;
 
 		Gdx.graphics.setForegroundFPS(fps);
+	}
+
+	public void useVsync(boolean vsync) {
+		this.vsync = vsync;
+		Gdx.graphics.setVSync(vsync);
+	}
+
+	public boolean isUsingVsync() {
+		return this.vsync;
 	}
 
 	public int getForegroundFPS() {
