966395a749cebc221a1228f36fc7cd6efc389df2
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:19:15 +0900

    デフォルトEvent追加

---
+++ desktop/src/com/tres/client/event/packet/DataPacketReceiveEvent.java
@@ -0,0 +1,9 @@
+package com.tres.client.event.packet;
+
+import com.tres.network.packet.DataPacket;
+
+public class DataPacketReceiveEvent extends PacketEvent {
+	public DataPacketReceiveEvent(DataPacket packet) {
+		super(packet);
+	}
+}
