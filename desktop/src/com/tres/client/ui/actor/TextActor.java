75449ba2b8c951a77631f9fd6dd97caa10c7ba9d
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:35:04 +0900

    TextActorを改良し、表示時間などを設定できるように

--- desktop/src/com/tres/client/ui/actor/TextActor.java
+++ desktop/src/com/tres/client/ui/actor/TextActor.java
@@ -1,28 +1,71 @@
 package com.tres.client.ui.actor;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Align;
 
 public class TextActor extends Actor {
+
+	public static TextActor annotation(Actor base, CharSequence text, BitmapFont font) {
+		TextActor actor = new TextActor(text, font);
+		actor.setPosition(base.getX(), base.getY() + 2, Align.topLeft);
+		return actor;
+	}
 
 	protected CharSequence text;
 
 	protected BitmapFont font;
-	protected Color color;
+	protected float duration;
 
-	public TextActor(CharSequence text, BitmapFont font, Color color) {
+	public TextActor(CharSequence text, BitmapFont font) {
 		this.text = text;
 		this.font = font;
-		this.color = color;
+		this.duration = -1f;
+	}
+
+	public TextActor(CharSequence text, BitmapFont font, float duration) {
+		this.text = text;
+		this.font = font;
+		this.duration = duration;
 	}
 
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
-
-		this.font.setColor(this.color);
 		this.font.draw(batch, this.text, getX(), getY());
+	}
+
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+
+		if (this.duration > 0f) {
+			this.duration -= delta;
+
+			if (this.duration <= 0f) {
+				this.remove();
+			}
+		}
+	}
+
+	public CharSequence getText() {
+		return text;
+	}
+
+	public BitmapFont getFont() {
+		return font;
+	}
+
+	public void setText(CharSequence text) {
+		this.text = text;
+	}
+
+	public float getDuration() {
+		return duration;
+	}
+
+	public void setDuration(float duration) {
+		this.duration = duration;
 	}
 }
