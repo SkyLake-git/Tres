528ea366be65d7fe5908543fc5403a9d65754355
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:36:29 +0900

    SettingsScreenを改良、vsyncの設定をできるように

--- desktop/src/com/tres/sequence/SettingsScreen.java
+++ desktop/src/com/tres/sequence/SettingsScreen.java
@@ -1,16 +1,23 @@
 package com.tres.sequence;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.tres.ScreenSequence;
 import com.tres.TresApplication;
+import com.tres.client.ui.WorldUtils;
 import com.tres.client.ui.actor.BorderlessButtonActor;
 import com.tres.client.ui.actor.PinSliderActor;
+import com.tres.client.ui.actor.ToggleButtonActor;
+import com.tres.client.ui.layout.NarrowLayout;
+import com.tres.client.ui.layout.attachment.LimitedViewAttachment;
 
 import java.util.ArrayList;
 
@@ -26,6 +33,12 @@ public class SettingsScreen extends ScreenSequence {
 
 	protected BorderlessButtonActor returnButton;
 
+	protected ToggleButtonActor vsyncButton;
+
+	protected NarrowLayout list;
+
+	protected LimitedViewAttachment viewAttachment;
+
 	public SettingsScreen(TresApplication game, Viewport viewport, Screen returnTo) {
 		super(game, viewport);
 		this.returnTo = returnTo;
@@ -35,16 +48,39 @@ public class SettingsScreen extends ScreenSequence {
 	protected void init() {
 		this.camera = new OrthographicCamera(getViewport().getScreenWidth(), getViewport().getScreenHeight());
 
+		this.list = new NarrowLayout(
+				WorldUtils.percentage(0.f, -0.35f),
+				300,
+				60 * 6,
+				Align.center,
+				30,
+				true
+		);
+
+		this.viewAttachment = new LimitedViewAttachment(6);
+		this.list.addAttachment(this.viewAttachment);
+
 		this.slider = new PinSliderActor(
 				new Vector2(0, 0),
-				300,
-				40,
+				400,
+				60,
 				5,
 				320,
 				1,
 				false,
 				new BitmapFont(),
 				"FPS: %.0f"
+		);
+
+		this.vsyncButton = new ToggleButtonActor(
+				new Vector2(0, 0),
+				new BorderlessButtonActor.Button(
+						"Use VSync",
+						180,
+						60,
+						new Color(0f, 0f, 0f, 1f),
+						0f
+				)
 		);
 
 
@@ -71,22 +107,22 @@ public class SettingsScreen extends ScreenSequence {
 		}
 
 		this.applyButton = new BorderlessButtonActor(
-				new Vector2(120, -200),
+				WorldUtils.percentage(0.1f, -0.5f).add(0, 40),
 				new BorderlessButtonActor.Button(
 						"Apply",
-						200,
-						40,
+						300,
+						60,
 						new Color(0, 0, 0, 1f),
 						0.25f
 				)
 		);
 
 		this.returnButton = new BorderlessButtonActor(
-				new Vector2(-120, -200),
+				WorldUtils.percentage(-0.1f, -0.5f).add(0, 40),
 				new BorderlessButtonActor.Button(
 						"Return",
-						200,
-						40,
+						300,
+						60,
 						new Color(0, 0, 0, 1f),
 						0f
 				)
@@ -97,6 +133,9 @@ public class SettingsScreen extends ScreenSequence {
 		this.stage.addActor(this.slider);
 		this.stage.addActor(this.applyButton);
 		this.stage.addActor(this.returnButton);
+		this.stage.addActor(this.vsyncButton);
+		this.list.add(this.slider);
+		this.list.add(this.vsyncButton);
 	}
 
 	@Override
@@ -115,12 +154,25 @@ public class SettingsScreen extends ScreenSequence {
 
 		if (this.applyButton.isPressed()) {
 			this.game.getSettings().setForegroundFPS((int) this.slider.getValue());
+			this.game.getSettings().useVsync(this.vsyncButton.getToggle());
 		}
 
 		if (this.returnButton.isPressed()) {
 			this.game.setScreen(this.returnTo);
 		}
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+			this.viewAttachment.scroll(1);
+		}
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+			this.viewAttachment.scroll(-1);
+		}
 	}
 
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
 
+	}
 }
