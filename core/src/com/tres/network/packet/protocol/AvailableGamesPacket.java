6dd439ccb4b8a5adf643c82a4ef1466e63b9ec28
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:26:17 +0900

    AvailableGameを共有するパケットの追加

--- core/src/com/tres/network/packet/protocol/AvailableGamesPacket.java
+++ core/src/com/tres/network/packet/protocol/AvailableGamesPacket.java
@@ -4,21 +4,44 @@ import com.tres.network.packet.Clientbound;
 import com.tres.network.packet.DataPacket;
 import com.tres.network.packet.PacketDecoder;
 import com.tres.network.packet.PacketEncoder;
+import com.tres.network.packet.protocol.types.AvailableGameInfo;
+
+import java.util.ArrayList;
 
 public class AvailableGamesPacket extends DataPacket implements Clientbound {
+
+	public ArrayList<AvailableGameInfo> games = new ArrayList<>();
+
+
+	public long timestamp;
+
 	@Override
 	protected void decodePayload(PacketDecoder in) throws Exception {
+		int length = in.readInt();
+
+		for (int i = 0; i < length; i++) {
+			this.games.add(AvailableGameInfo.read(in));
+		}
+
+
+		this.timestamp = in.readLong();
 
 	}
 
 	@Override
 	protected void encodePayload(PacketEncoder out) throws Exception {
+		out.writeInt(this.games.size());
 
+		for (AvailableGameInfo gameInfo : this.games) {
+			gameInfo.write(out);
+		}
+
+		out.writeLong(this.timestamp);
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return "AvailableGamesPacket";
 	}
 
 	@Override
