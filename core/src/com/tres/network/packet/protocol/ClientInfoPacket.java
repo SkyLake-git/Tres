8d974a452b4eb4a1d97b177d67289549b616246e
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:27:58 +0900

    ClientInfoPacketは追加でClientInfoを含むように

--- core/src/com/tres/network/packet/protocol/ClientInfoPacket.java
+++ core/src/com/tres/network/packet/protocol/ClientInfoPacket.java
@@ -1,22 +1,23 @@
 package com.tres.network.packet.protocol;
 
-import com.tres.network.packet.DataPacket;
-import com.tres.network.packet.PacketDecoder;
-import com.tres.network.packet.PacketEncoder;
-import com.tres.network.packet.Serverbound;
+import com.tres.network.packet.*;
 
 public class ClientInfoPacket extends DataPacket implements Serverbound {
 
 	public String remoteAddress;
 
+	public ClientInfo info;
+
 	@Override
 	protected void decodePayload(PacketDecoder in) throws Exception {
 		this.remoteAddress = in.readString();
+		this.info = ClientInfo.read(in);
 	}
 
 	@Override
 	protected void encodePayload(PacketEncoder out) throws Exception {
 		out.writeString(this.remoteAddress);
+		this.info.write(out);
 	}
 
 	@Override
