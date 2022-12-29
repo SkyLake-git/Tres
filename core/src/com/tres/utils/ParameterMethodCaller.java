32910729d3dd61f085538c9d9a7327148a072468
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-12-29 23:20:23 +0900

    ParameterMethodCallerの追加。自動で指定パラメーターのメソッドを呼び出してくれるように

---
+++ core/src/com/tres/utils/ParameterMethodCaller.java
@@ -0,0 +1,47 @@
+package com.tres.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+public class ParameterMethodCaller<E> {
+
+	protected HashMap<Class<E>, Method> map;
+
+	protected Object target;
+
+	public ParameterMethodCaller(Object target) {
+		this.map = new HashMap<>();
+		this.target = target;
+
+		this.analyze();
+	}
+
+	private void analyze() {
+		for (Method method : this.target.getClass().getMethods()) {
+			for (Class<?> paramType : method.getParameterTypes()) {
+				try {
+					Class<E> tmp = (Class<E>) paramType; // fixme: hack
+				} catch (Throwable e) {
+					continue;
+				}
+
+				this.map.put((Class<E>) paramType, method);
+				break;
+			}
+		}
+	}
+
+	public void call(E param) throws Throwable {
+		Method method = this.map.get(param.getClass());
+		if (method != null) {
+			try {
+				method.invoke(this.target, param);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw e.getCause();
+			}
+		}
+	}
+}
