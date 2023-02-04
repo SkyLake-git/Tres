b5c4ef3f119c188df5e21decd7a6bd0df374227e
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:21:40 +0900

    AliveSignalPacket: メソッドシグネチャ更新

--- uno/core/src/com/tres/network/packet/protocol/AliveSignalPacket.java
+++ uno/core/src/com/tres/network/packet/protocol/AliveSignalPacket.java
@@ -2,17 +2,19 @@ package com.tres.network.packet.protocol;
 
 import com.tres.network.packet.*;
 
+import java.io.IOException;
+
 public class AliveSignalPacket extends DataPacket implements Clientbound, Serverbound {
 
 	public int isig = 0;
 
 	@Override
-	protected void decodePayload(PacketDecoder in) throws Exception {
+	protected void decodePayload(PacketDecoder in) throws InvalidPayloadException, IOException {
 		this.isig = in.readInt();
 	}
 
 	@Override
-	protected void encodePayload(PacketEncoder out) throws Exception {
+	protected void encodePayload(PacketEncoder out) throws InvalidPayloadException, IOException {
 		out.getStream().writeInt(this.isig);
 	}
 
