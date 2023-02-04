93ac136f68f8973c39b453c30d47b43d9435996b
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:23:34 +0900

    CardInfoはプレイヤーを持たないように、setCountを削除

--- uno/core/src/com/tres/network/uno/CardInfo.java
+++ uno/core/src/com/tres/network/uno/CardInfo.java
@@ -2,24 +2,14 @@ package com.tres.network.uno;
 
 public class CardInfo {
 
-	protected Player player;
 
 	protected int count;
 
-	CardInfo(Player player) {
-		this.player = player;
+	public CardInfo() {
 		this.count = 0;
-	}
-
-	public Player getPlayer() {
-		return player;
 	}
 
 	public int getCount() {
 		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
 	}
 }
