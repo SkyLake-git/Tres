215880600b8a51c3b49c74cc25d657b13e811c89
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:30:40 +0900

    PlayerAction::actionOf を削除、交換方法が変更されて軽量化されたため

--- uno/core/src/com/tres/network/packet/protocol/types/PlayerAction.java
+++ uno/core/src/com/tres/network/packet/protocol/types/PlayerAction.java
@@ -2,21 +2,12 @@ package com.tres.network.packet.protocol.types;
 
 public enum PlayerAction {
 
-	JOIN_GAME(1);
+	JOIN_GAME(1),
+	LEAVE_GAME(2);
 
 	public final int id;
 
 	PlayerAction(int action) {
 		this.id = action;
-	}
-
-	public static PlayerAction actionOf(int id) {
-		for (PlayerAction e : values()) {
-			if (e.id == id) {
-				return e;
-			}
-		}
-
-		return null;
 	}
 }
