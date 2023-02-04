198b26bfd5ab030abcc8282db51ac769b2dac7e0
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:28:55 +0900

    PlayerInfoRequestPacket: メソッドシグネチャの更新と deprecatedタグの付与

--- uno/core/src/com/tres/network/packet/protocol/PlayerInfoRequestPacket.java
+++ uno/core/src/com/tres/network/packet/protocol/PlayerInfoRequestPacket.java
@@ -1,21 +1,23 @@
 package com.tres.network.packet.protocol;
 
-import com.tres.network.packet.DataPacket;
-import com.tres.network.packet.PacketDecoder;
-import com.tres.network.packet.PacketEncoder;
-import com.tres.network.packet.Serverbound;
+import com.tres.network.packet.*;
 
+import java.io.IOException;
+
+/**
+ * @deprecated
+ */
 public class PlayerInfoRequestPacket extends DataPacket implements Serverbound {
 
 	public String name;
 
 	@Override
-	protected void decodePayload(PacketDecoder in) throws Exception {
+	protected void decodePayload(PacketDecoder in) throws InvalidPayloadException, IOException {
 		this.name = in.readUTFString();
 	}
 
 	@Override
-	protected void encodePayload(PacketEncoder out) throws Exception {
+	protected void encodePayload(PacketEncoder out) throws InvalidPayloadException, IOException {
 		out.writeUTFString(this.name);
 	}
 
