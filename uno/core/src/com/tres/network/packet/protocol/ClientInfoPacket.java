8aa7dfb91b32ef88d1316bf7f646d2c6c714a251
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:24:27 +0900

    ClientInfoPacketのメソッドシグネチャ更新

--- uno/core/src/com/tres/network/packet/protocol/ClientInfoPacket.java
+++ uno/core/src/com/tres/network/packet/protocol/ClientInfoPacket.java
@@ -2,6 +2,8 @@ package com.tres.network.packet.protocol;
 
 import com.tres.network.packet.*;
 
+import java.io.IOException;
+
 public class ClientInfoPacket extends DataPacket implements Serverbound {
 
 	public String remoteAddress;
@@ -9,13 +11,13 @@ public class ClientInfoPacket extends DataPacket implements Serverbound {
 	public ClientInfo info;
 
 	@Override
-	protected void decodePayload(PacketDecoder in) throws Exception {
+	protected void decodePayload(PacketDecoder in) throws InvalidPayloadException, IOException {
 		this.remoteAddress = in.readUTFString();
 		this.info = ClientInfo.read(in);
 	}
 
 	@Override
-	protected void encodePayload(PacketEncoder out) throws Exception {
+	protected void encodePayload(PacketEncoder out) throws InvalidPayloadException, IOException {
 		out.writeUTFString(this.remoteAddress);
 		this.info.write(out);
 	}
