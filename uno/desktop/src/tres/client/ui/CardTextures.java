da0395796e35698b18ce82fd3225633bc5865051
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:32:41 +0900

    CardTextures: クラスイニシャライザを削除し、::initに移動、Indexの計算方法がバカほど間違っている問題を修正

--- uno/desktop/src/tres/client/ui/CardTextures.java
+++ uno/desktop/src/tres/client/ui/CardTextures.java
@@ -4,7 +4,6 @@ package tres.client.ui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.tres.network.uno.Card;
-import com.tres.network.uno.CardValidator;
 
 import java.util.HashMap;
 
@@ -12,9 +11,12 @@ public class CardTextures {
 
 	public static final HashMap<Integer, Texture> MAP = new HashMap<>();
 
+	public static final int WIDTH = 161;
+	public static final int HEIGHT = 266;
+
 
 	public static int getIndex(Card.Symbol symbol, Card.Color color) {
-		return symbol.i * color.ordinal();
+		return symbol.hashCode() + color.hashCode();
 	}
 
 	public static String getImageName(Card.Symbol symbol, Card.Color color) {
@@ -24,20 +26,29 @@ public class CardTextures {
 		}
 
 
-		if (Card.Symbol.isWild(symbol)) {
+		if (symbol.isWild()) {
 			second = "wild";
 		}
-		return color.toEnglishName() + "_" + symbol + ".png";
+		return second + "/" + "card_" + color.toEnglishName() + "_" + symbol + ".png";
 	}
 
 	private static void register(Card.Symbol symbol, Card.Color color) {
-		CardTextures.MAP.put(getIndex(symbol, color), new Texture(Gdx.files.internal(getImageName(symbol, color))));
+		int ind = getIndex(symbol, color);
+		if (MAP.containsKey(ind)) {
+			throw new RuntimeException("contains");
+		}
+
+		CardTextures.MAP.put(ind, new Texture(Gdx.files.internal("card/" + getImageName(symbol, color))));
 	}
 
-	static {
+	public static void init() {
+		if (MAP.size() > 0) {
+			return;
+		}
+
 		for (Card.Symbol symbol : Card.Symbol.values()) {
 			for (Card.Color color : Card.Color.values()) {
-				if (CardValidator.validate(symbol, color)) {
+				if ((new Card(symbol, color)).isValidCombination()) {
 					register(symbol, color);
 				}
 			}
