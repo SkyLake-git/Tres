a06a183821681959166a307cde58f2e96d511b85
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:36:23 +0900

    NetworkCardの改良

--- uno/core/src/com/tres/network/uno/NetworkCard.java
+++ uno/core/src/com/tres/network/uno/NetworkCard.java
@@ -1,6 +1,22 @@
 package com.tres.network.uno;
 
+import com.tres.network.packet.PacketDecoder;
+import com.tres.network.packet.PacketEncoder;
+import com.tres.utils.Utils;
+
+import java.io.IOException;
+
 public class NetworkCard extends Card {
+
+	private static int nextId = 0;
+
+	public static int nextRuntimeId() {
+		return nextId++;
+	}
+
+	public static NetworkCard by(int runtimeId, Card card) {
+		return new NetworkCard(runtimeId, card.symbol, card.color);
+	}
 
 
 	public final int runtimeId;
@@ -9,5 +25,19 @@ public class NetworkCard extends Card {
 		super(symbol, color);
 
 		this.runtimeId = runtimeId;
+	}
+
+	public void write(PacketEncoder out) throws IOException {
+		out.writeInt(this.runtimeId);
+		out.writeInt(this.symbol.ordinal());
+		out.writeInt(this.color.ordinal());
+	}
+
+	public static NetworkCard read(PacketDecoder in) throws IOException, IndexOutOfBoundsException {
+		int runtimeId = in.readInt();
+		int symbolOrdinal = in.readInt();
+		int colorOrdinal = in.readInt();
+
+		return new NetworkCard(runtimeId, Utils.getEnumOrdinal(Symbol.class, symbolOrdinal), Utils.getEnumOrdinal(Color.class, colorOrdinal));
 	}
 }
