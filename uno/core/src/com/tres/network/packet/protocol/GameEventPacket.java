8f01a9f5f27cc92302cf6e07611cb4376f6f2c5a
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-04 23:25:53 +0900

    GameEventPacket: 問題の修正と改良、イベントの追加

--- uno/core/src/com/tres/network/packet/protocol/GameEventPacket.java
+++ uno/core/src/com/tres/network/packet/protocol/GameEventPacket.java
@@ -1,38 +1,42 @@
 package com.tres.network.packet.protocol;
 
-import com.tres.network.packet.Clientbound;
-import com.tres.network.packet.DataPacket;
-import com.tres.network.packet.PacketDecoder;
-import com.tres.network.packet.PacketEncoder;
+import com.tres.network.packet.*;
+
+import java.io.IOException;
 
 public class GameEventPacket extends DataPacket implements Clientbound {
 
+	public static final int START = 0x02;
+
+	public static final int CLOSE = 0x04;
+
 	public static final int IN_GAME = 0x01;
+	public static final int SYSTEM = 0x02;
 
 	public static final int TURN_START = 0x01 | IN_GAME;
 	public static final int TURN_END = 0x02 | IN_GAME;
+	public static final int SOMEONE_OUT = 0x04 | IN_GAME; // 誰かがあがった (カードを使い切った)
+	public static final int END = 0x08 | IN_GAME; // 一人を除いた全員が上がった (全員の順位が決まった) これは実質的なゲームの終了イベントです
+	public static final int UNO_SHOUT = 0x16 | IN_GAME;
+	public static final int UNO_ACCUSATION = 0x32 | IN_GAME;
+
+	public static final int PLAYER_JOIN = 0x01 | SYSTEM;
+	public static final int PLAYER_LEFT = 0x02 | SYSTEM;
 
 	public int event;
 
-	public int playerRuntimeId = -1;
+	public short playerRuntimeId = -1;
 
 	@Override
-	protected void decodePayload(PacketDecoder in) throws Exception {
+	protected void decodePayload(PacketDecoder in) throws InvalidPayloadException, IOException {
 		this.event = in.readInt();
-		boolean hasPlayer = in.getStream().readBoolean();
-		if (hasPlayer) {
-			this.playerRuntimeId = in.getStream().readShort();
-		}
+		this.playerRuntimeId = in.readNaturalShort();
 	}
 
 	@Override
-	protected void encodePayload(PacketEncoder out) throws Exception {
+	protected void encodePayload(PacketEncoder out) throws InvalidPayloadException, IOException {
 		out.writeInt(this.event);
-		out.getStream().writeShort(this.playerRuntimeId);
-		out.getStream().writeBoolean(this.playerRuntimeId >= 0);
-		if (this.playerRuntimeId >= 0) {
-			out.getStream().writeShort(this.playerRuntimeId);
-		}
+		out.writeNaturalNumber(this.playerRuntimeId);
 	}
 
 	@Override
